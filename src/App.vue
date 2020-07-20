eda51137f4cbdb99a6bd4f539c183796ab24c21f
Ankarin <ankarn41k@gmail.com>
2020-07-20 16:48:37 +0300

    table for simlpe project

--- src/App.vue
+++ src/App.vue
@@ -1,189 +1,59 @@
 <template>
-  <v-app>
-    <v-data-table
-      :headers="headers"
-      :items="devs"
-      sort-by="calories"
-      class="elevation-1"
-    >
-      <template v-slot:top>
-        <v-toolbar flat>
-          <v-toolbar-title>My CRUD</v-toolbar-title>
-          <v-divider class="mx-4" inset vertical></v-divider>
-          <v-spacer></v-spacer>
-          <v-dialog v-model="dialog" max-width="500px">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn color="dark" dark class="mb-2" v-bind="attrs" v-on="on"
-                >New Item</v-btn
-              >
-            </template>
-            <v-card>
-              <v-card-title>
-                <span class="headline">{{ formTitle }}</span>
-              </v-card-title>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" dark app>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-email</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.name"
-                        label="Name"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.time"
-                        label="Time"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.companyRate"
-                        label="Company rate"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.devRate"
-                        label="Dev rate"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field
-                        v-model="editedItem.fee"
-                        label="Fee %"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
+    <v-app-bar app dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Project 1</v-toolbar-title>
+    </v-app-bar>
 
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-toolbar>
-      </template>
-      <template v-slot:item.actions="{ item }">
-        <v-icon small class="mr-2" @click="editItem(item)">
-          mdi-pencil
-        </v-icon>
-        <v-icon small @click="deleteItem(item)">
-          mdi-delete
-        </v-icon>
-      </template>
-      <template v-slot:no-data>
-        <v-btn color="primary" @click="initialize">Reset</v-btn>
-      </template>
-    </v-data-table>
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <ProjectSpace class="projectSpace" />
+      </v-container>
+    </v-main>
+    <v-footer app dark>
+      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
+import ProjectSpace from "./components/ProjectSpace.vue";
 export default {
+  components: {
+    ProjectSpace
+  },
+  props: {
+    source: String
+  },
   data: () => ({
-    dialog: false,
-    headers: [
-      {
-        text: "Name",
-        align: "start",
-        sortable: false,
-        value: "name"
-      },
-      { text: "Time", align: "start", value: "time" },
-      { text: "Company rate", align: "start", value: "companyRate" },
-      { text: "Dev rate", align: "start", value: "devRate" },
-      { text: "Fee %", align: "start", value: "fee" },
-      { text: "Margin per hour", align: "start", value: "marginPerH" },
-      { text: "Employee payout", align: "start", value: "devPayout" },
-      { text: "Margin", align: "start", value: "margin" },
-      { text: "Actions", align: "start", value: "actions", sortable: false }
-    ],
-    devs: [],
-    editedIndex: -1,
-    editedItem: {
-      name: "",
-      time: 0,
-      companyRate: 0,
-      devRate: 0,
-      fee: 0
-    },
-    defaultItem: {
-      name: "",
-      time: 0,
-      companyRate: 0,
-      devRate: 0,
-      fee: 0,
-      marginPerH: 0,
-      devPayout: 0,
-      margin: 0
-    }
-  }),
-
-  computed: {
-    formTitle() {
-      return this.editedIndex === -1 ? "New Developer" : "Edit";
-    }
-  },
-
-  watch: {
-    dialog(val) {
-      val || this.close();
-    }
-  },
-
-  created() {
-    this.initialize();
-  },
-
-  methods: {
-    initialize() {
-      this.devs = [
-        {
-          name: "Nasya Pisya",
-          time: 13.5,
-          companyRate: 25,
-          devRate: 15,
-          fee: 5,
-          marginPerH: 0,
-          devPayout: 0,
-          margin: 0
-        }
-      ];
-    },
-
-    editItem(item) {
-      this.editedIndex = this.devs.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
-    },
-
-    deleteItem(item) {
-      const index = this.devs.indexOf(item);
-      confirm("Are you sure you want to delete this item?") &&
-        this.devs.splice(index, 1);
-    },
-
-    close() {
-      this.dialog = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
-    },
-
-    save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.devs[this.editedIndex], this.editedItem);
-      } else {
-        this.devs.push(this.editedItem);
-      }
-      this.close();
-    }
-  }
+    drawer: null
+  })
 };
 </script>
+
+<style lang="scss">
+.projectSpace {
+  width: 100%;
+}
+</style>
---
+++ src/components/ProjectSpace.vue
@@ -0,0 +1,270 @@
+<template>
+  <v-app>
+    <v-data-table
+      :headers="headers"
+      :items="devs"
+      sort-by="calories"
+      class="elevation-1"
+    >
+      <template v-slot:top>
+        <v-toolbar flat dark>
+          <v-toolbar-title>Project 1</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="500px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn dark class="mb-2" v-bind="attrs" v-on="on"
+                >New Developer</v-btn
+              >
+            </template>
+            <v-card>
+              <v-card-title class="headOfEdit">
+                <span class="headline">{{ formTitle }}</span>
+                <v-checkbox
+                  v-model="editedItem.isEmpFixedPayment"
+                  label="Fixed salary"
+                ></v-checkbox>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.name"
+                        label="Name"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.time"
+                        label="Time"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.companyRate"
+                        label="Company rate"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        :disabled="editedItem.isEmpFixedPayment"
+                        v-model="editedItem.devRate"
+                        value="0"
+                        label="Dev rate"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.fee"
+                        label="Fee %"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-if="!editedItem.isEmpFixedPayment"
+                        disabled
+                        :value="calcDevPayout.toFixed(2)"
+                        label="Dev payout"
+                      ></v-text-field>
+                      <v-text-field
+                        v-if="editedItem.isEmpFixedPayment"
+                        v-model="editedItem.devPayout"
+                        label="Dev payout"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        disabled
+                        :value="calcMarginPerH.toFixed(2)"
+                        label="Margin per hour"
+                      ></v-text-field>
+                    </v-col>
+
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        disabled
+                        :value="calcMargin.toFixed(2)"
+                        label="Margin"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-icon small class="mr-2" @click="editItem(item)">
+          mdi-pencil
+        </v-icon>
+        <v-icon small @click="deleteItem(item)">
+          mdi-delete
+        </v-icon>
+      </template>
+      <template v-slot:no-data>
+        <v-btn color="primary" @click="initialize">Reset</v-btn>
+      </template>
+    </v-data-table>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    dialog: false,
+    kek: 15,
+    headers: [
+      {
+        text: "Name",
+        align: "start",
+        sortable: false,
+        value: "name"
+      },
+      { text: "Time", align: "start", value: "time" },
+      { text: "Company rate", align: "start", value: "companyRate" },
+      { text: "Dev rate", align: "start", value: "devRate" },
+      { text: "Fee %", align: "start", value: "fee" },
+      { text: "Margin per hour", align: "start", value: "marginPerH" },
+      { text: "Dev payout", align: "start", value: "devPayout" },
+      { text: "Margin", align: "start", value: "margin" },
+      { text: "Actions", align: "start", value: "actions", sortable: false }
+    ],
+    devs: [
+      {
+        name: "Nasya Pisya",
+        time: 13.5,
+        companyRate: 25,
+        devRate: 15,
+        fee: 5,
+        marginPerH: 5,
+        devPayout: 0,
+        margin: 0,
+        eployeeFixedPayment: 0,
+        isEmpFixedPayment: false
+      }
+    ],
+    editedIndex: -1,
+    editedItem: {
+      name: "",
+      time: 0,
+      companyRate: 0,
+      devRate: 0,
+      fee: 0,
+      marginPerH: 0,
+      isEmpFixedPayment: false,
+      devPayout: 0,
+      margin: 0
+    },
+    defaultItem: {
+      name: "",
+      time: 0,
+      companyRate: 0,
+      devRate: 0,
+      fee: 0,
+      marginPerH: 0,
+      isEmpFixedPayment: false,
+      devPayout: 0,
+      margin: 0
+    }
+  }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Developer" : "Edit";
+    },
+    calcMarginPerH() {
+      return this.calcMargin / this.editedItem.time;
+    },
+    calcDevPayout() {
+      return this.editedItem.devRate * this.editedItem.time;
+    },
+    calcMargin() {
+      if (this.editedItem.isEmpFixedPayment) {
+        return (
+          ((this.editedItem.companyRate * this.editedItem.time) / 100) *
+            (100 - this.editedItem.fee) -
+          this.editedItem.devPayout
+        );
+      } else
+        return (
+          ((this.editedItem.companyRate * this.editedItem.time) / 100) *
+            (100 - this.editedItem.fee) -
+          this.editedItem.devRate * this.editedItem.time
+        );
+    },
+    calcCustomerPaid() {
+      return this.editedItem.companyRate * this.editedItem.time;
+    }
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    }
+  },
+
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+    initialize() {},
+
+    editItem(item) {
+      this.editedIndex = this.devs.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      const index = this.devs.indexOf(item);
+      confirm("Are you sure you want to delete this item?") &&
+        this.devs.splice(index, 1);
+    },
+
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      let edited = this.editedItem;
+      edited.marginPerH = this.calcMarginPerH.toFixed(2);
+      if (!this.editedItem.isEmpFixedPayment) {
+        edited.devPayout = this.calcDevPayout.toFixed(2);
+      }
+
+      edited.margin = this.calcMargin.toFixed(2);
+      if (this.editedItem.isEmpFixedPayment) {
+        edited.devRate = 0;
+      }
+      if (this.editedIndex > -1) {
+        Object.assign(this.devs[this.editedIndex], edited);
+      } else {
+        this.devs.push(edited);
+      }
+      this.close();
+    }
+  }
+};
+</script>
+<style lang="scss">
+.headOfEdit {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
